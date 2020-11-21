894452adc2e3f924e21fc4002b7432ef5ab1ebbf
Ranganath Venkataraman <rangys3@gmail.com>
2020-11-21 18:45:05 +0000

    Add files via upload

---
+++ score_11_21.py
@@ -0,0 +1,21 @@
+import json
+import numpy as np
+import os
+import joblib, pickle
+from azureml.core import Model
+import xgboost
+from xgboost import XGBRegressor
+
+
+def init():
+    model_path = Model.get_model_path('registered.sav')
+    model = joblib.load(model_path)
+
+def run(data):
+    try:
+        result = model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
